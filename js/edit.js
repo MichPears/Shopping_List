9a8107fe7ae1b18ee489f1cb3760e653e30b1653
Michelle <mich.pearsall@gmail.com>
2022-06-27 13:33:13 -0600

    js separated and layout finished

--- js/edit.js
+++ js/edit.js
@@ -1,12 +1,12 @@
 import {
-  toggleDarkMode,
   modeBtn,
+  toggleDarkModeEdit,
   toggleModeIcon,
 } from "./modules/dark_mode_toggle.js";
 
-let addForm = document.querySelector(".shopping-form");
-
 modeBtn.addEventListener("click", () => {
   toggleModeIcon();
-  toggleDarkMode();
+  toggleDarkModeEdit();
 });
+
+toggleDarkModeEdit();
--- js/modules/dark_mode_toggle.js
+++ js/modules/dark_mode_toggle.js
@@ -1,34 +1,50 @@
 const modeBtn = document.querySelector(".day-night-btn");
 const label = document.querySelector(".label");
 const addItemInput = document.querySelector(".text-input");
-const addBtn = document.querySelector(".btn");
+const btns = document.querySelectorAll(".btn");
 const bodyBg = document.querySelector(".body");
 const headerBg = document.querySelector(".header-container");
 const logo = document.querySelector(".logo");
 const subtitles = document.querySelectorAll(".subtitle");
 const links = document.querySelectorAll(".link");
+const editSubtitle = document.querySelector(".edit-subtitle");
+const themeIconBtn = document.querySelector(".day-night-btn");
+const editInput = document.querySelector(".edit-input");
+
+const toggleGeneral = () => {
+  label.classList.toggle("label-dark");
+  btns.forEach((btn) => btn.classList.toggle("btn-dark"));
+  themeIconBtn.classList.toggle("day-night-btn-dark");
+};
 
 const toggleDarkMode = () => {
-  label.classList.toggle("label-dark");
-  addItemInput.classList.toggle("text-input-dark");
-  addBtn.classList.toggle("btn-dark");
-  modeBtn.classList.toggle("day-night-btn-dark");
+  toggleGeneral();
   bodyBg.classList.toggle("body-dark");
+  addItemInput.classList.toggle("text-input-dark");
   headerBg.classList.toggle("header-container-dark");
   logo.classList.toggle("logo-dark");
   subtitles.forEach((subtitle) => subtitle.classList.toggle("subtitle-dark"));
   links.forEach((link) => link.classList.toggle("link-dark"));
 };
 
-export {
-  toggleDarkMode,
-  modeBtn,
-  label,
-  addItemInput,
-  addBtn,
-  bodyBg,
-  headerBg,
-  logo,
-  subtitles,
-  links,
+const toggleDarkModeEdit = () => {
+  toggleGeneral();
+  editInput.classList.toggle("edit-input-dark");
+  editSubtitle.classList.toggle("edit-subtitle-dark");
+  bodyBg.classList.toggle("body-dark");
+  headerBg.classList.toggle("header-container-dark");
+  logo.classList.toggle("logo-dark");
 };
+let defaultMode = true;
+
+const toggleModeIcon = () => {
+  if (defaultMode) {
+    modeBtn.innerHTML = `<span class="material-symbols-outlined"> dark_mode </span>`;
+    defaultMode = false;
+  } else {
+    modeBtn.innerHTML = `<span class="material-symbols-outlined"> light_mode </span>`;
+    defaultMode = true;
+  }
+};
+
+export { toggleDarkMode, toggleDarkModeEdit, toggleModeIcon, modeBtn };
